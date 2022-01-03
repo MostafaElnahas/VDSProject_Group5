10cc0ee1f6eff7809934b1f9bdcc1b7c82fbe9ac
MostafaElnahas <94486559+MostafaElnahas@users.noreply.github.com>
2022-01-03 12:40:40 +0100

    data

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,1 +1,50 @@
 #include "Manager.h"
+#include <vector>
+#include <iostream>
+#include <experimental/iterator>
+
+using namespace std;
+
+namespace ClassProject{
+
+struct rows{
+       char label;
+       int IDD,High,Low,TopVar;
+           };
+    vector<rows> table;
+void filltable  (char Label, int idd,int high,int low,int topvar ) {
+
+
+
+table.push_back({Label,idd,high,low,topvar});
+
+
+}
+void print ( )
+{
+
+
+    for (const auto &table : table) {
+        cout << "First Name: " << table.label << endl
+             << "Last Name: " << table.IDD << endl
+             << "Roll Number: " << table.High << endl;
+    }
+}
+
+
+
+};
+
+int main(){
+    ClassProject::filltable('a', 0 , 1, 0 , 0);
+    ClassProject::print();
+    cout<<"hello word"<<endl;
+
+};
+
+
+
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,6 @@
 
 int main(int argc, char* argv[])
 {
-    ClassProject::createtable() ;
+
 
 }
