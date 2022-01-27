44460b916a7ef0ce78a30f44d48bb081e76efb8a
MostafaElnahas <94486559+MostafaElnahas@users.noreply.github.com>
2022-01-27 20:08:11 +0100

    correcting

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Carolina P. Nogueira 2016
+//
+
+#include <iostream>
+#include <string>
+#include "Manager.h"
+#include "gtest/gtest.h"
+
+
+
+int main(int argc, char* argv[])
+{
+
+
+}
