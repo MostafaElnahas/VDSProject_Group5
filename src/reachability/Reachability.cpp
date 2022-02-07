15fd07f2d3451351e3b6c348458a2f3693b3f3d1
MostafaElnahas <94486559+MostafaElnahas@users.noreply.github.com>
2022-02-07 18:29:30 +0100

    .,.,

--- src/reachability/Reachability.cpp
+++ src/reachability/Reachability.cpp
@@ -4,19 +4,29 @@
 //
 
 #include "Reachability.h"
-ClassProject::ReachabilityInterface::ReachabilityInterface() {
 
-
-
-
-}
-
-
-    }
-
-
-
-}
 ClassProject::Reachability::Reachability(unsigned int stateSize) : ReachabilityInterface(stateSize){
 
 }
+
+const vector<ClassProject::BDD_ID> & ClassProject::Reachability::getStates()  {
+
+}
+
+bool ClassProject::Reachability::isReachable(const std::vector<bool> &stateVector) {
+
+
+}
+
+void ClassProject::Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) {
+
+
+}
+void ClassProject::Reachability::setInitState(const std::vector<bool> &stateVector) {
+
+
+}
+
+
+
+
